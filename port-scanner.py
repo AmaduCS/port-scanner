c658f9461c416887529e9f1bbf7258d6e67b2586
AmaduCS <amadubalde.cyber@gmail.com>
2025-06-17 18:09:25 +0100

    added command line arguments using argparse and updated the readme file

--- port-scanner.py
+++ port-scanner.py
@@ -1,6 +1,7 @@
 # A simple port scanning tool with my favorite language
 import socket 
 import time 
+import argparse
 
 def scan_ports(target, start_port, end_port):
     print(f"Scanning {target} from port {start_port} to {end_port}...\n")
@@ -22,7 +23,15 @@ def scan_ports(target, start_port, end_port):
 
 
 if __name__ == "__main__":
-    target = input("Enter the target IP or Domain: ")
-    start_port = int(input("Start Port: "))
-    end_port = int(input("End Port: "))
-    scan_ports(target, start_port, end_port)
+    # target = input("Enter the target IP or Domain: ")
+    # start_port = int(input("Start Port: "))
+    # end_port = int(input("End Port: "))
+
+    parser = argparse.ArgumentParser(description="Simple Python Port Scanner")
+    parser.add_argument("-t", "--target", required=True, help="Target Ip or domain")
+    parser.add_argument("-s", "--start", type=int, required=True, help="Starting Port")
+    parser.add_argument("-e", "--end", type=int, required=True, help="Ending port")
+
+    args = parser.parse_args()
+
+    scan_ports(args.target, args.start, args.end)
